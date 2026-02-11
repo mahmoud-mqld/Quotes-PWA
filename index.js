6dba8b44eda44ade247fcdf4f0971cc424216042
Mahmoud <mqldm@yahoo.com>
2026-02-11 16:17:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function change() {
     used = [];
   }
   do {
-    var r = Math.floor(Math.random() * quotes.length);
+    var r =+1;
   } while (used.includes(r));
 
   document.getElementById("caption").innerHTML =
@@ -146,6 +146,7 @@ function change() {
   used.push(r);
   console.log(used);
 }
+
 
 
 
